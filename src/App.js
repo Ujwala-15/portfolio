e1d0097d563cbff1f6d943bdd61b26c6a7b9cd5a
Ujwala-15 <ujwala.manjunath@gmail.com>
2021-10-16 01:12:06 +0530

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./componnents/Navbar";
+import Header from "./componnents/Header";
+import Particles from "react-particles-js";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
